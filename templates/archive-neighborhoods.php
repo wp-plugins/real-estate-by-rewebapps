e59ce439420fcece645d5a11f1e22d6f63ca0ea9
bhubbard <bhubbard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-12 19:52:57 +0000

    Update to 1.5.5
    git-svn-id: https://plugins.svn.wordpress.org/real-estate-by-rewebapps/trunk@856610 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/archive-neighborhoods.php
+++ templates/archive-neighborhoods.php
@@ -2,21 +2,16 @@
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-		
+
 	<?php  if (is_post_type_archive()) { ?>
 		<h1 class="pagetitle"><?php post_type_archive_title() ?></h1>
  	<?php } ?>
-		
-		<?php if (have_posts()) : ?>
-		 <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<?php $posts=query_posts($query_string . '&orderby=title&order=asc'); 
-		  while (have_posts()) : the_post(); ?>
-		  
 		  <h2><?php the_title(); ?></h2>
-		  
-		  
+
+
 		<div class="neighborhood">
 			<div class="neighborhood-thumb">
 				<a href="<?php the_permalink() ?>">
@@ -32,7 +27,7 @@
 		      	  	<li><h3><a href="<?php the_permalink() ?>" rel="bookmark" title="More about <?php the_title(); ?>"><?php get_the_title(); ?></a></h3></li>
 					<li><?php the_excerpt(); ?></li>
 				</ul>
-				
+
 			</div>
 			<div class="neighborhood-read-more"><a href="<?php the_permalink(); ?>">View Neighborhood</a></div>
 			<div class="clear"></div>
@@ -40,29 +35,15 @@
 
 		<?php endwhile; ?>
 
-		<?php neighborhood_paginate(); ?>
-	<?php else :
 
-		if ( is_category() ) { // If this is a category archive
-			printf("<h2 class='center'>".__("Sorry, but there aren't any posts in the %s category yet.", 'kubrick').'</h2>', single_cat_title('',false));
-		} else if ( is_date() ) { // If this is a date archive
-			echo('<h2>'.__("Sorry, but there aren't any posts with this date.", 'kubrick').'</h2>');
-		} else if ( is_author() ) { // If this is a category archive
-			$userdata = get_userdatabylogin(get_query_var('author_name'));
-			printf("<h2 class='center'>".__("Sorry, but there aren't any posts by %s yet.", 'kubrick')."</h2>", $userdata->display_name);
-		} else {
-			echo("<h2 class='center'>".__('No posts found.', 'kubrick').'</h2>');
-		}
-	  get_search_form();
-	endif;
-?>
+	<?php else : endif; ?>
 
 
 	</div>
  </div>
-  
-  
-  
+
+
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
 
